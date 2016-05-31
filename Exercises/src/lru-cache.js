2c95f2d4e21f60142ff88903bcb38ec4b70339c6
Jeff Dean <jeff@galvanize.com>
2016-05-30 23:46:19 -0600

    added lru cache example

---
+++ Exercises/src/lru-cache.js
@@ -0,0 +1,6 @@
+// Implement a LRU cache
+// Every time you `get` a key, it marks it as most-recently used
+// Every time you `put` an item, it expires the least-used item
+
+module.exports = function (maxSize) {
+}
