cbc994d5046ec1d57c3e341ecb6f233b2a3dad51
Tyler Bettilyon <teb311@gmail.com>
2016-07-11 14:34:12 -0700

    provided more information on how to get started for the queue exercise

--- Exercises/src/queue.js
+++ Exercises/src/queue.js
@@ -3,5 +3,7 @@
 // Implement a queue using a doubly-linked list with a head/tail pointer
 // Make enqueue(), dequeue() and size() all run in O(1) time
 
+// See test/queue.test.js, this function, when called, should return a new object
+// which represents a queue. 
 module.exports = function(){
 }
