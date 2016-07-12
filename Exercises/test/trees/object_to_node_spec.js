b304d964d215fe7b57a006f5e11bd0b46a77540f
Liz Howard <liz.howard@galvanize.com>
2016-07-12 15:08:35 -0700

    added multi-child test

--- Exercises/test/trees/object_to_node_spec.js
+++ Exercises/test/trees/object_to_node_spec.js
@@ -40,6 +40,54 @@ describe('objectToNode', function() {
     expect(rootNode.children[0].name).to.deep.equal('b');
     expect(rootNode.children[0]).to.be.an.instanceof(Node);
     expect(rootNode.children[0].children[0].name).to.deep.equal('c');
+    expect(rootNode.children[0].children[0]).to.be.an.instanceof(Node);
     expect(rootNode.children[0].children[0].children[0].name).to.deep.equal('d');
+    expect(rootNode.children[0].children[0].children[0]).to.be.an.instanceof(Node);
+  });
+
+  it("handles deeply nested objects with more than one child", function () {
+    var input = {
+      name: 'a',
+      children: [
+        {
+          name: 'b',
+          children: [
+            {
+              name: 'c',
+              children: [
+                {
+                  name: 'd',
+                  children: []
+                },
+                {
+                  name: '3',
+                  children: []
+                }
+              ]
+            },
+            {
+              name: '2',
+              children: []
+            }
+          ]
+        },
+        {
+          name: '1',
+          children: []
+        }
+      ]
+    };
+    var rootNode = objectToNode(input);
+    expect(rootNode.name).to.deep.equal('a');
+    expect(rootNode).to.be.an.instanceof(Node);
+    expect(rootNode.children[0].name).to.deep.equal('b');
+    expect(rootNode.children[1].name).to.deep.equal('1');
+    expect(rootNode.children[0]).to.be.an.instanceof(Node);
+    expect(rootNode.children[0].children[0].name).to.deep.equal('c');
+    expect(rootNode.children[0].children[1].name).to.deep.equal('2');
+    expect(rootNode.children[0].children[0]).to.be.an.instanceof(Node);
+    expect(rootNode.children[0].children[0].children[0].name).to.deep.equal('d');
+    expect(rootNode.children[0].children[0].children[1].name).to.deep.equal('3');
+    expect(rootNode.children[0].children[0].children[0]).to.be.an.instanceof(Node);
   });
 });
