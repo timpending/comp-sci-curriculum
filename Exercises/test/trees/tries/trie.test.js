fb803da68028c7912f01916b2ce5c11d16ec53e5
Tyler Bettilyon <teb311@gmail.com>
2016-07-20 15:17:24 -0700

    fixed a test in tries where the description did not match the test.

--- Exercises/test/trees/tries/trie.test.js
+++ Exercises/test/trees/tries/trie.test.js
@@ -78,7 +78,7 @@ describe("Trie", function() {
 
       it("gets its own node if it is a word", function(){
         t.learn("a");
-        expect(t.characters.a.getWords()).to.deep.equal([""]);
+        expect(t.characters.a.getWords()).to.deep.equal(["a"]);
       });
 
       it("returns an empty array if there are no words", function(){
